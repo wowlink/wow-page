8208cc894fd8538fe89101d0748a7d19e9e8907b
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-22 01:38:35 -0700

    feat: install wow packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { useLocation, BrowserRouter as Router } from "react-router-dom";
 
-function App() {
+// http://localhost:3000&wow=github
+// window.location.href = "https://github.com";
+
+const fakeAsync = (milliseconds: number) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+const useQuery = (): URLSearchParams => {
+  return new URLSearchParams(useLocation().search);
+};
+
+const Home = (): JSX.Element => {
+  const query = useQuery();
+  const wowlink = query.get("wow");
+  const [progress, setProgress] = useState({ msg: "initiation" });
+
+  useEffect(() => {
+    const convertAndRedirect = async () => {
+      setProgress({ msg: "fetch mappings" });
+      await fakeAsync(2000);
+      setProgress({ msg: "redirect 🚀🚀🚀" });
+    };
+    if (!wowlink) {
+      setProgress({ msg: "wowlink not found 🔥🔥🔥" });
+      return;
+    }
+    convertAndRedirect();
+  }, [wowlink]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          I am too lazy to change the default home page ¯\_(ツ)_/¯
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          wowlink &&
+          <p>
+            WowLink: {wowlink}
+          </p>
+        }
+        <p>
+          Progress: {progress.msg}
+        </p>
       </header>
     </div>
   );
-}
+};
+
+const App = (): JSX.Element => {
+  return (
+    <Router>
+      <Home />
+    </Router>
+  );
+};
 
 export default App;
