f859e4f5707bd5b73638b7203ddb25bf9242ec20
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-23 19:27:18 -0700

    fix: test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders wowlink page', () => {
   render(<App />);
   const linkElement = screen.getByText(/wowlink/i);
   expect(linkElement).toBeInTheDocument();
