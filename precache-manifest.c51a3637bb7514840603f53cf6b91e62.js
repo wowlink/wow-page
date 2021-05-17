5a657812bd91bea480443503a8bd9e032ec9508a
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-17 03:53:09 +0000

    Updates

---
+++ precache-manifest.c51a3637bb7514840603f53cf6b91e62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c7d13ccad1f4217c0579a8b6b3d7c2",
+    "url": "/wow-page/index.html"
+  },
+  {
+    "revision": "c2c78aaf39f795aff529",
+    "url": "/wow-page/static/css/main.25122b0a.chunk.css"
+  },
+  {
+    "revision": "f64716850ec1b8a74a94",
+    "url": "/wow-page/static/js/2.f18e4220.chunk.js"
+  },
+  {
+    "revision": "f529c5786bd5b853914e",
+    "url": "/wow-page/static/js/3.f7e15c81.chunk.js"
+  },
+  {
+    "revision": "c2c78aaf39f795aff529",
+    "url": "/wow-page/static/js/main.b61d0e68.chunk.js"
+  },
+  {
+    "revision": "8b7179fd94c3c2044427",
+    "url": "/wow-page/static/js/runtime~main.27017b8f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/wow-page/static/media/logo.06e73328.svg"
+  }
+]);
